340c108a314c75e9eefa2c206cacd8d9cbeb6ba2
Utsav Gurmachhan - Main <utsavmagar88728@gmail.com>
2023-03-13 08:28:44 +0545

    Create Dependency Injection

---
+++ workshop.Infrastructure/DI/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using workshop.Application.Common.Interface;
+using workshop.Infrastructure.Persistence;
+
+namespace workshop.Infrastructure.DI
+{
+    public class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDBContext>(
+                options => options.UseNpgsql(configuration.GetConnectionString("WorkshopDatabasePostgreSQL"),
+                b => b.MigrationsAssembly(typeof(ApplicationDBContext).Assembly.FullName)), ServiceLifetime.Transient);
+
+            services.AddScoped<IApplicationDBContext>(provider => (IApplicationDBContext)provider.GetServices<ApplicationDBContext>());
+            //services.AddTransient<IDateTime, DateTimeService>();
+            return services;
+        }
+    }
+}
