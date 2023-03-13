0e5d8d33fbd779935b4fef0b3c2776e8517c0ec4
Utsav Gurmachhan - Main <utsavmagar88728@gmail.com>
2023-03-13 08:16:06 +0545

    Create ApplicationDBContext in Persistence

---
+++ workshop.Infrastructure/Persistence/ApplicationDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using workshop.Application.Common.Interface;
+using workshop.Domain.Entities;
+
+namespace workshop.Infrastructure.Persistence
+{
+    public class ApplicationDBContext : DbContext, IApplicationDBContext
+    {
+        private readonly IDateTime _dateTime;
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options, IDateTime dateTime) : base(options)
+        {
+            _dateTime = dateTime;
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+
+
+    }
+}
