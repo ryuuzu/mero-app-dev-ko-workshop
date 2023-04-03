9f44f82eaf0adfcfc654386bbcf3818ff04854b7
Utsav Gurmachhan <utsavmagar88728@gmail.com>
2023-04-03 07:31:10 +0545

    Added interface for authentication

---
+++ workshop.Application/Common/Interface/IAuthentication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using workshop.Application.DTOs;
+
+namespace workshop.Application.Common.Interface
+{
+    public interface IAuthentication
+    {
+        Task<ResponseDTO> Register(UserRegisterRequestDTO model);
+    }
+}
