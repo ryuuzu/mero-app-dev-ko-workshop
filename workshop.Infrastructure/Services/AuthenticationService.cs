061f1bbc7f2fe5e1aa99cdd426bbbe37a6c3db1a
Utsav Gurmachhan <utsavmagar88728@gmail.com>
2023-04-03 07:44:46 +0545

    Implemented register in authentication service

--- workshop.Infrastructure/Services/AuthenticationService.cs
+++ workshop.Infrastructure/Services/AuthenticationService.cs
@@ -18,10 +18,32 @@ namespace workshop.Infrastructure.Services
             _userManager = userManager;
         }
 
-        public Task<ResponseDTO> Register(UserRegisterRequestDTO model)
+        public async Task<ResponseDTO> Register(UserRegisterRequestDTO model)
         {
-            throw new NotImplementedException();
-        }
+            var userExists = await _userManager.FindByNameAsync(model.Username);
+            if (userExists != null)
+            {
+                return new ResponseDTO { Status = "Error", Message = "User already exists!" };
 
+            }
+
+            IdentityUser user = new()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username
+            };
+
+            var result = await _userManager.CreateAsync(user);
+            if (!result.Succeeded)
+            {
+                return new ResponseDTO
+                {
+                    Status = "Error",
+                    Message = "User creation failed! Please check user details."
+                };
+            }
+            return new ResponseDTO { Status = "Success", Message = "User created successfully!" };
+        }
     }
 }
