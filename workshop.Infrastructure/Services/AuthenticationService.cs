146e6d1c1e5a8f2efb1e0a0aebe49ce36d34ed85
Utsav Gurmachhan <utsavmagar88728@gmail.com>
2023-04-03 07:36:38 +0545

    Added service for authentication

---
+++ workshop.Infrastructure/Services/AuthenticationService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using workshop.Application.Common.Interface;
+using workshop.Application.DTOs;
+
+namespace workshop.Infrastructure.Services
+{
+    public class AuthenticationService : IAuthentication
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthenticationService(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public Task<ResponseDTO> Register(UserRegisterRequestDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
