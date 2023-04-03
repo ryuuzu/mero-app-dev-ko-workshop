91b59b8abe37fdf3bf05a30e3ddd18437b8aefeb
Utsav Gurmachhan <utsavmagar88728@gmail.com>
2023-04-03 08:02:46 +0545

    Added controller and routes for authentication

---
+++ workshop.api/Controllers/AuthenticateController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using workshop.Application.Common.Interface;
+using workshop.Application.DTOs;
+
+namespace workshop.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IAuthentication _authenticationManger;
+
+        private AuthenticateController(UserManager<IdentityUser> userManager, IAuthentication authenticationManager)
+        {
+            _userManager = userManager;
+            _authenticationManger = authenticationManager;
+        }
+
+        [HttpPost]
+        [Route("/api/authenticate/register")]
+        public async Task<ResponseDTO> Register([FromBody] UserRegisterRequestDTO model)
+        {
+            var result = await _authenticationManger.Register(model);
+            return result;
+        }
+    }
+}
--- workshop.api/Program.cs
+++ workshop.api/Program.cs
@@ -23,6 +23,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
